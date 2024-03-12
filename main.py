f00de9c35e57ca8aeaede354e329addc6728b301
hhilsber <63950960+hhilsber@users.noreply.github.com>
2024-03-12 15:25:19 +0900

    start

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
 
-print('lessgo')
+is_cuda = torch.cuda.is_available()
+print("Cuda?  is_available: {} --- version: {}".format(is_cuda,torch.version.cuda))
+
+# GPU or CPU
+if is_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+print("device: {}".format(device))
