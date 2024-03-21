2c6e0db42c721f9b3da8745a9d45598e5fef20bf
hhilsber <63950960+hhilsber@users.noreply.github.com>
2024-03-21 13:47:56 +0900

    cleaning

--- src/pipeline.py
+++ src/pipeline.py
@@ -39,20 +39,16 @@ class Pipeline(object):
         if config['type_train'] == 'dky':
             print('type_train: dont know yet')
 
-        # Data loader
-        print("      train set = whole dataset for the moment")
-        #train_set = Data(x=dataset['features'], edge_index=dataset['edge_list'])
-        #self.train_loader = DataLoader([train_set], batch_size=config['batch_size'], shuffle=False)
-        #train_set = NormalDataset(dataset['features'], dataset['adjacency'])
-        #self.train_loader = DataLoader(train_set, batch_size=config['nbr_nodes'], shuffle=False)
-        
-        print("       train set = batch size?")
 
     def type_train(self):
         print("train")
         self.run_training(training=True)
 
     def run_training(self, training=True):
+        x = self.train_feat.to(self.device)
+        adj = self.train_adj.to(self.device)
+        
+        # Epoch
         for epoch in range(self.config['max_iter']):
             print(' train epoch: {}/{}'.format(epoch+1, self.config['max_iter']))
             self.model.edge_module.train()
@@ -60,26 +56,9 @@ class Pipeline(object):
             if training:
                 self.model.optimizer.zero_grad()
             
-            x = self.train_feat.to(self.device)
-            adj = self.train_adj.to(self.device)
-            
-            self.model.optimizer.zero_grad()
-
             output = self.model.edge_module(x, adj)
             
             loss = self.criterion(output)
             loss.backward()
             self.model.optimizer.step()      
-            """
-            pred = torch.round(output)
-
-            # Compute confusion vector between 2 tensors
-            confusion_vector = pred / target
-            # Compute validation f1 score
-            f1_train = compute_metrics(confusion_vector, print_values=False)
-            f1_train_history.append(f1_train)
-            
-            loss.backward()
-            optimizer.step()         
-            loss_float = loss.item()
-            loss_train_history.append(loss_float)"""
+        
