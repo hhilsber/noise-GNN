00b4b965b45931331c4e891195ed6e2f1cf5d4c6
Hendrik Hilsberg <hhilsberg@login01.murata-lab.internal>
2024-04-24 15:21:29 +0900

    batch 2

--- tbatch1.sh
+++ tbatch1.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=test_job1              # Job name
 #SBATCH --mem=16g                         # Job memory request
 #SBATCH --gres=gpu:1                     # Number of requested GPU(s) 
-#SBATCH --time=1:00:00                   # Time limit days-hrs:min:sec
+#SBATCH --time=6:00:00                   # Time limit days-hrs:min:sec
 #SBATCH --error=../out_batch/slurm1.err                # Error file name
 #SBATCH --output=../out_batch/slurm1.out               # Output file name
 
--- tbatch3.sh
+++ tbatch3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-#SBATCH --job-name=test_job2              # Job name
+#SBATCH --job-name=test_job3              # Job name
 #SBATCH --mem=16g                         # Job memory request
 #SBATCH --gres=gpu:1                     # Number of requested GPU(s) 
 #SBATCH --time=6:00:00                   # Time limit days-hrs:min:sec
-#SBATCH --error=../out_batch/slurm2.err                # Error file name
-#SBATCH --output=../out_batch/slurm2.out               # Output file name
+#SBATCH --error=../out_batch/slurm3.err                # Error file name
+#SBATCH --output=../out_batch/slurm3.out               # Output file name
 
 source /opt/conda/etc/profile.d/conda.sh      # initialize conda
 conda activate pyl                  # load up the conda environment
-python main.py -config config/config_products2.yml                      # launch the code
+python main.py -config config/config_products3.yml                      # launch the code
