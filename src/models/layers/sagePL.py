40d80681dc9a1c8535a4458d6e026fb4c0985967
Hendrik Hilsberg <hhilsberg@login01.murata-lab.internal>
2024-07-22 13:31:01 +0900

    kedal

--- src/models/layers/sagePL.py
+++ src/models/layers/sagePL.py
@@ -30,14 +30,14 @@ class SAGEPL(torch.nn.Module):
         for conv in self.convs:
             conv.reset_parameters()
 
-    def forward(self, x, edge_index, edge_rewire, noisy_rate=0.3, n_id=None):
+    def forward(self, x, edge_index, edge_rewire, noisy_rate=0.1, n_id=None):
 
         x_pure, y_pure, z_pure = self.forward_pure(x, edge_index)
         noisy_x = self.adding_noise(x, noisy_rate=noisy_rate, n_id=n_id)
         x_noisy, y_noisy, z_noisy  = self.forward_noisy(noisy_x, edge_rewire)
 
         return x_pure, y_pure, z_pure, x_noisy, y_noisy, z_noisy
-
+ 
     def adding_noise(self, x, noisy_rate, n_id=None):
         noisy_x = x.clone()
         if n_id is None:
