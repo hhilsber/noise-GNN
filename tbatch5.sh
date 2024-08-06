835d12fa5e6d5f9b2753b8e8f89d98a5028f659f
Hendrik Hilsberg <hhilsberg@login01.murata-lab.internal>
2024-08-06 21:46:19 +0900

    config

--- tbatch5.sh
+++ tbatch5.sh
@@ -2,10 +2,13 @@
 #SBATCH --job-name=test_job5              # Job name
 #SBATCH --mem=24g                         # Job memory request
 #SBATCH --gres=gpu:1                     # Number of requested GPU(s) 
-#SBATCH --time=24:00:00                   # Time limit days-hrs:min:sec
+#SBATCH --time=36:00:00                   # Time limit days-hrs:min:sec
 #SBATCH --error=../out_batch/slurm5.err                # Error file name
 #SBATCH --output=../out_batch/slurm5.out               # Output file name
 
 source /opt/conda/etc/profile.d/conda.sh      # initialize conda
 conda activate pyl                  # load up the conda environment
-python main.py -config config/config_arxiv.yml
+python main.py -config config/config_arxiv5.yml
+python main.py -config config/config_arxiv6.yml
+python main.py -config config/config_arxiv7.yml
+python main.py -config config/config_arxiv8.yml
